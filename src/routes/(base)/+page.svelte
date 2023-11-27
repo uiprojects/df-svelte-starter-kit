a227053faa754c61ec869e4c86d150f2d2a485ad
Nanthakumar J J <jjnanthakumar477@gmail.com>
2023-11-27 16:40:22 +0530

    Added footer for Header layout - TopMENU location

--- src/routes/(base)/+page.svelte
+++ src/routes/(base)/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { P } from 'flowbite-svelte';
+	import { P, Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
 
 	// Create a config object with type of the object, Embed details and Token Type
 	// export let data;
@@ -17,4 +17,32 @@
 	<P class="text-2xl lg:text-2xl text-center font-extrabold sm:px-16 xl:px-40 dark:text-gray-400"
 		>Welcome to DF Svelte Starter Kit demonstration</P
 	>
+	<Table>
+		<TableHead>
+		  <TableHeadCell>Product name</TableHeadCell>
+		  <TableHeadCell>Color</TableHeadCell>
+		  <TableHeadCell>Category</TableHeadCell>
+		  <TableHeadCell>Price</TableHeadCell>
+		</TableHead>
+		<TableBody class="divide-y">
+		  <TableBodyRow>
+			<TableBodyCell>Apple MacBook Pro 17"</TableBodyCell>
+			<TableBodyCell>Sliver</TableBodyCell>
+			<TableBodyCell>Laptop</TableBodyCell>
+			<TableBodyCell>$2999</TableBodyCell>
+		  </TableBodyRow>
+		  <TableBodyRow>
+			<TableBodyCell>Microsoft Surface Pro</TableBodyCell>
+			<TableBodyCell>White</TableBodyCell>
+			<TableBodyCell>Laptop PC</TableBodyCell>
+			<TableBodyCell>$1999</TableBodyCell>
+		  </TableBodyRow>
+		  <TableBodyRow>
+			<TableBodyCell>Magic Mouse 2</TableBodyCell>
+			<TableBodyCell>Black</TableBodyCell>
+			<TableBodyCell>Accessories</TableBodyCell>
+			<TableBodyCell>$99</TableBodyCell>
+		  </TableBodyRow>
+		</TableBody>
+	  </Table>
 </div>
